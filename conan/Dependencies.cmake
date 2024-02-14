d5291c3651807edfadb14a0ff37a538557096f20
Amir Nourinia <amir.nouri.nia@gmail.com>
2024-02-14 07:46:07 +0100

    add glad as submodule

--- conan/Dependencies.cmake
+++ conan/Dependencies.cmake
@@ -1,2 +1,13 @@
 find_package(Boost REQUIRED COMPONENTS system)
 include_directories(${Boost_INCLUDE_DIRS})
+
+find_package(GLFW3 REQUIRED)
+
+# Path to glad directory
+set(GLAD_SOURCES_DIR "${CMAKE_SOURCE_DIR}/conan/external/glad")
+# Path to glad cmake files
+add_subdirectory("${GLAD_SOURCES_DIR}/cmake" glad_cmake)
+
+# Specify glad settings
+set(GLAD_LIBRARY_NAME glad_gl_core_41)
+glad_add_library(${GLAD_LIBRARY_NAME} REPRODUCIBLE MX API gl:core=4.1)
