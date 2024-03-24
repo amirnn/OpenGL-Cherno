e96c29f7f19c4ca091915d136159e3f71a89aa91
Amir Nourinia <a.nourinia@holo-light.com>
2024-03-25 00:39:34 +0100

    Same

--- src/Utils/GLCall.cpp
+++ src/Utils/GLCall.cpp
@@ -11,8 +11,8 @@ bool GLLogCall(char const* function, char const* file, int line)
 {
   while (uint error = glGetError())
   {
-    std::cout << "[OpenGL Error] : (" << error << "), " << function << \
-        file << "line: " << line << std::endl;
+    std::cout << "[OpenGL Error] : (0x0" << std::hex << error << "), " << std::dec \
+              << function << file << "line: " << line << std::endl;
     if (error == GL_NO_ERROR)
     {
       return false;
