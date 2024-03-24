b81b85fa09f182b999f26465a3cb8ac956316fef
Amir Nourinia <a.nourinia@holo-light.com>
2024-03-24 11:39:28 +0100

    Use the Generated Glad

--- conan/Dependencies.cmake
+++ conan/Dependencies.cmake
@@ -4,26 +4,42 @@
 find_package(GLFW3 REQUIRED)
 include_directories(${glfw3_INCLUDE_DIRS})
 
-# Path to glad directory
-set(GLAD_SOURCES_DIR "${CMAKE_SOURCE_DIR}/conan/external/glad")
-# Path to glad cmake files
-add_subdirectory("${GLAD_SOURCES_DIR}/cmake" glad_cmake)
-include_directories()
-
 find_package(glm REQUIRED)
 include_directories(${glm_INCLUDE_DIRS})
 
 find_package(Eigen3 REQUIRED)
 include_directories(${Eigen3_INCLUDE_DIRS})
 
-# Specify glad settings
-# Look at GladConfig.cmake file for better understanding
-if(APPLE)
-    set(GLAD_LIBRARY_NAME glad_gl_core_41)
-    glad_add_library(${GLAD_LIBRARY_NAME} REPRODUCIBLE API gl:core=4.1)
-elseif(WIN32)
-    set(GLAD_LIBRARY_NAME glad_gl_core_46)
-    glad_add_library(${GLAD_LIBRARY_NAME} API gl:core=4.6)
-#    glad_add_library(${GLAD_LIBRARY_NAME} MX API gl:core=4.1)
-endif(APPLE)
+if(BUILD_GLAD_FROM_SOURCES)
+    # Path to glad directory
+    set(GLAD_SOURCES_DIR "${CMAKE_SOURCE_DIR}/conan/external/glad")
+    # Path to glad cmake files
+    add_subdirectory("${GLAD_SOURCES_DIR}/cmake" glad_cmake)
+
+    # Specify glad settings
+    # Look at GladConfig.cmake file for better understanding
+    if(APPLE)
+        set(GLAD_LIBRARY_NAME glad_gl_core_41)
+        glad_add_library(${GLAD_LIBRARY_NAME} API gl:core=4.1)
+        add_compile_definitions(APPLE_SYSTEM)
+    elseif(WIN32)
+        set(GLAD_LIBRARY_NAME glad_gl_core_46)
+        glad_add_library(${GLAD_LIBRARY_NAME} API gl:core=4.6)
+    endif()
+else()
+    if(APPLE)
+        set(GLAD_SRC "${CMAKE_SOURCE_DIR}/conan/external/glad-generated/ogl-core-41-all-extensions-alias/src/gl.c")
+        set(GLAD_INCLUDE "${CMAKE_SOURCE_DIR}/conan/external/glad-generated/ogl-core-41-all-extensions-alias/include")
+        add_compile_definitions(APPLE_SYSTEM)
+    elseif(WIN32)
+        set(GLAD_SRC "${CMAKE_SOURCE_DIR}/conan/external/glad-generated/ogl-core-46-all-extensions-alias/src/gl.c")
+        set(GLAD_INCLUDE "${CMAKE_SOURCE_DIR}/conan/external/glad-generated/ogl-core-46-all-extensions-alias/include")
+    endif()
+    set(GLAD_LIBRARY_NAME glad_gl_core_all_extensions_alias)
+    add_library(${GLAD_LIBRARY_NAME} STATIC ${GLAD_SRC})
+    include_directories(${GLAD_INCLUDE})
+endif()
+
+
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -84,9 +84,16 @@ int main(void) {
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
+    #if defined(APPLE_SYSTEM)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #else
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #endif
 
     GLFWwindow *window = glfwCreateWindow(640, 480, "OpenGL Triangle", NULL, NULL);
     if (!window) {
